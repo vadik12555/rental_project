ee7464e1d853f42e3a047abfbdd370e48b221561
Vadik <vadik12555@yandex.ru>
2026-04-06 20:22:51 +0300

    feat: enhance Order admin with list filters and editable statuses

--- app/catalog/admin.py
+++ app/catalog/admin.py
@@ -3,4 +3,10 @@ from .models import Item  , Order
 
 admin.site.register(Item)
 # Register your models here.
-admin.site.register(Order)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'status', 'created_at') # Добавили status
+    list_filter = ('status', 'created_at') # Добавили фильтр справа
+    list_editable = ('status',) # Позволяет менять статус прямо в списке!
--- app/catalog/models.py
+++ app/catalog/models.py
@@ -15,12 +15,29 @@ class Item(models.Model):
         return self.title
 
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Ожидает оплаты'),
+        ('paid', 'Оплачено'),
+        ('shipped', 'Доставлено'),
+        ('canceled', 'Отменено'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE , related_name='orders', verbose_name="Покупатель")
     items = models.ManyToManyField(Item, related_name='orders', verbose_name="Товары")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Общая стоимость")
-    
+
     status = models.CharField(max_length=20, default='pending')
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+    def __str__(self):
+        return f"Заказ #{self.id} | {self.user.username} | {self.get_status_display()}"
+
+    
     def save(self, *args, **kwargs):
         is_new = self.pk is None
         super().save(*args, **kwargs)
