5469a142b9ef6c9f58abc97ff5b95750020fe842
Vadik <vadik12555@yandex.ru>
2026-04-07 18:26:16 +0300

    feat: implement stock management and auto-calculatetotal price on order creation

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -41,7 +41,12 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         is_new = self.pk is None
         super().save(*args, **kwargs)
-        if not is_new:
+        if  is_new:
+            for item in self.items.all():
+                if item.stock > 0:
+                    item.stock -= 1 
+                    item.save()
+
             self.update_total_price()
 
     def update_total_price(self):
@@ -58,3 +63,13 @@ class Meta:
 
     def __str__(self):
         return f"Заказ №{self.id} от {self.user.username}"
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=10, verbose_name="Остаток на складе")
+        
+    def __str__(self):
+        return f"{self.name} (Осталось: {self.stock})"
