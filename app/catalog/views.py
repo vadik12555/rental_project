94c279a87053c95728ceaef8fedcfd35c9a03691
Vadik <vadik12555@yandex.ru>
2026-04-09 16:40:51 +0300

    feat: настроена JWT-авторизация, API заказов и сохранение медиа в Docker

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,20 +1,9 @@
-from rest_framework import viewsets, permissions
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Item, Order
 from .serializers import ItemSerializer, OrderSerializer
 
-class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-    # Фильтры убрали, так как библиотека не установилась
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
 
 def item_list(request):
     items = Item.objects.all()
@@ -24,3 +13,25 @@ def item_list(request):
     else:
         items = Item.objects.all()
     return render(request, 'catalog/index.html', {'items': items})
+
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+
+
+# --- API ДЛЯ ЗАКАЗОВ (С ЗАЩИТОЙ) 
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Чтобы каждый видел только свои заказы
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # ГЛАВНАЯ ДОБАВКА: привязываем заказ к юзеру из токена
+        serializer.save(user=self.request.user)
