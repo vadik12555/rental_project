e6b3d2c3369c9acfd32ddfb3d59e5a2d2c54f5bf
Vadik <vadik12555@yandex.ru>
2026-04-16 17:59:39 +0300

    фикс импортов, urls и redis. сервер запущен

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.views import APIView     
+from rest_framework.response import Response  
 from rest_framework import viewsets, permissions, generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Item, Order
--- app/core/urls.py
+++ app/core/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from catalog.views import ItemViewSet, OrderViewSet, item_list
+from catalog.views import ItemViewSet, OrderViewSet, item_list, CartAPIView 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
@@ -16,6 +17,7 @@ urlpatterns = [
     path('shop/', item_list, name='shop'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('cart/', CartAPIView.as_view(), name='cart_api'), # УБЕДИСЬ, ЧТО ТУТ НЕТ ОШИБОК В СКОБКАХ
 ]
 
 # Чтобы работали картинки, если ты их загружаешь
