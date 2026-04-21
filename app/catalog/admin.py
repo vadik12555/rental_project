a12d541f05e213e46fc7cbbdfe01c34055ef8afc
Vadik <vadik12555@yandex.ru>
2026-04-21 16:26:45 +0300

    админка: добавил поиск, фильтры и редактирование остатков прямо в списке

--- app/catalog/admin.py
+++ app/catalog/admin.py
@@ -10,3 +10,17 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'total_price', 'status', 'created_at') # Добавили status
     list_filter = ('status', 'created_at') # Добавили фильтр справа
     list_editable = ('status',) # Позволяет менять статус прямо в списке!
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'stock') # Выводим остатки
+    search_fields = ('title',) # Поиск по названию
+    list_editable = ('price', 'stock') # Меняем прямо в списке
+    eadonly_fields = ('get_image_preview',)
+    
+    def get_image_preview(self, obj):
+        if obj.image:
+            from django.utils.safestring import mark_safe
+            return mark_safe(f'<img src="{obj.image.url}" width="100" />')
+        return "Нет фото"
--- app/core/settings.py
+++ app/core/settings.py
@@ -159,3 +159,8 @@ CACHES = {
         }
     }      
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
