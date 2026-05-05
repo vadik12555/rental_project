d96036676b2faf2f912dc7fa85321cc6abb78d7b
Vadik <vadik12555@yandex.ru>
2026-05-05 17:16:11 +0300

    celery and redis

--- app/core/celery.py
+++ app/core/celery.py
@@ -1,14 +1,18 @@
+from __future__ import annotations
+
 import os
+
 from celery import Celery
 
-# Устанавливаем настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Создаем объект Celery (переменная должна называться именно app)
 app = Celery('core')
 
-# Загружаем конфиг из settings.py с префиксом CELERY
-app.config_from_object('django.conf.settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Автопоиск задач tasks.py
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
